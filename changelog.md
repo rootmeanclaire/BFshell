d9d0d15698deca68e03b3012334ade3917875f52
FracturedRetina <admin@krakendev.net>
2014-04-22 21:00:40 -0700

    Added icon

--- changelog.md
+++ changelog.md
@@ -1,14 +1,21 @@
 Beta
 ====
+
 1.0.1
 -----
  * Removed debug features
  	* Output integer value of character after character
  	* Output newline after input
+
 1.1.0
 -----
  * Fixed `Syntax error: missing ']'` message displaying unecessarily
+
 1.2
 ---
  * Fixed `Syntax error: missing ']'` message displaying unecessarily in other situations
  * Added texet commands
+
+1.2.1
+-----
+ * Added icon
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "parser.h"
 
 /**************************\
- * Version: 1.2 Beta      *
+ * Version: 1.2.1 Beta    *
  * Author: Evan Shimoniak *
 \**************************/
 
@@ -55,7 +55,7 @@ void processCommand(char* line) {
 	if (strcmp(line, "help\n") == 0) {
 		printf("COMMANDS\n\t\"version\" Print the current version of the interpreter\n\t\"reset\"   Reset program memory\n");
 	} else if (strcmp(line, "version\n") == 0) {
-		printf("Beta 1.1.0\n");
+		printf("Beta 1.2.1\n");
 	} else if (strcmp(line, "reset\n") == 0) {
 		reset();
 		setColor(BLACK, DEFAULT);
